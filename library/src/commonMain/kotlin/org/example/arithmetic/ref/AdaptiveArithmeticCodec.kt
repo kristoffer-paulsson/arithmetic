71507ae0a417354ec1c60cb1618d5493521819a1
Kristoffer Paulsson <kristoffer.paulsson@talenten.se>
2025-08-23 01:08:56 +0200

    Imported and made adaptive arithmetic codec

---
+++ library/src/commonMain/kotlin/org/example/arithmetic/ref/AdaptiveArithmeticCodec.kt
@@ -0,0 +1,45 @@
+/*
+ * Reference arithmetic coding
+ *
+ * Copyright (c) Project Nayuki
+ * MIT License. See readme file.
+ * https://www.nayuki.io/page/reference-arithmetic-coding
+ */
+
+package org.example.arithmetic.ref
+
+import org.example.arithmetic.ref.ArithmeticDecoder.read
+
+public object AdaptiveArithmeticCodec {
+
+    // To allow unit testing, this method is package-private instead of private.
+    fun compress(src: BitInputBuffer, dst: BitOutputBuffer) {
+        val initFreqs = FlatFrequencyTable(257)
+        val freqs: FrequencyTable = SimpleFrequencyTable(initFreqs)
+        val enc = ArithmeticEncoder(32, dst)
+        while (true) {
+            // Read and encode one byte
+            val symbol: Int = src.read()
+            if (symbol == -1) break
+            enc.write(freqs, symbol)
+            freqs.increment(symbol)
+        }
+        enc.write(freqs, 256) // EOF
+        enc.finish() // Flush remaining code bits
+    }
+
+    // To allow unit testing, this method is package-private instead of private.
+    fun decompress(src: BitInputBuffer, dst: BitOutputBuffer) {
+        val initFreqs = FlatFrequencyTable(257)
+        val freqs: FrequencyTable = SimpleFrequencyTable(initFreqs)
+        val dec = ArithmeticDecoder(32, src)
+        while (true) {
+            // Decode and write one byte
+            val symbol = dec.read(freqs)
+            if (symbol == 256)  // EOF symbol
+                break
+            dst.write(symbol)
+            freqs.increment(symbol)
+        }
+    }
+}
