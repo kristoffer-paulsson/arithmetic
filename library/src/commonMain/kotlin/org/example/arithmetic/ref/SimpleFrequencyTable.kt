824b14ba5c96354dd44243d47d96d2b6b1c14052
Kristoffer Paulsson <kristoffer.paulsson@talenten.se>
2025-08-23 00:55:35 +0200

    Added flat frequency table

---
+++ library/src/commonMain/kotlin/org/example/arithmetic/ref/SimpleFrequencyTable.kt
@@ -0,0 +1,194 @@
+package org.example.arithmetic.ref
+
+
+/**
+ * A mutable table of symbol frequencies. The number of symbols cannot be changed
+ * after construction. The current algorithm for calculating cumulative frequencies
+ * takes linear time, but there exist faster algorithms such as Fenwick trees.
+ */
+public class SimpleFrequencyTable : FrequencyTable {
+    /*---- Fields ----*/ // The frequency for each symbol. Its length is at least 1, and each element is non-negative.
+    private val frequencies: IntArray
+
+    // cumulative[i] is the sum of 'frequencies' from 0 (inclusive) to i (exclusive).
+    // Initialized lazily. When this is not null, the data is valid.
+    private var cumulative: IntArray = intArrayOf()
+
+    // Always equal to the sum of 'frequencies'.
+    private var total: Int
+
+
+    /*---- Constructors ----*/
+    /**
+     * Constructs a frequency table from the specified array of symbol frequencies. There must be at least
+     * 1 symbol, no symbol has a negative frequency, and the total must not exceed `Integer.MAX_VALUE`.
+     * @param freqs the array of symbol frequencies
+     * @throws NullPointerException if the array is `null`
+     * @throws IllegalArgumentException if `freqs.length` &lt; 1,
+     * `freqs.length` = `Integer.MAX_VALUE`, or any element `freqs[i]` &lt; 0
+     * @throws ArithmeticException if the total of `freqs` exceeds `Integer.MAX_VALUE`
+     */
+    public constructor(freqs: IntArray) {
+        require(freqs.size >= 1) { "At least 1 symbol needed" }
+        require(freqs.size <= Int.Companion.MAX_VALUE - 1) { "Too many symbols" }
+
+        frequencies = freqs.clone() // Make copy
+        total = 0
+        for (x in frequencies) {
+            require(x >= 0) { "Negative frequency" }
+            total += x
+        }
+    }
+
+
+    /**
+     * Constructs a frequency table by copying the specified frequency table.
+     * @param freqs the frequency table to copy
+     * @throws NullPointerException if `freqs` is `null`
+     * @throws IllegalArgumentException if `freqs.getSymbolLimit()` &lt; 1
+     * or any element `freqs.get(i)` &lt; 0
+     * @throws ArithmeticException if the total of all `freqs` elements exceeds `Integer.MAX_VALUE`
+     */
+    public constructor(freqs: FrequencyTable) {
+        val numSym = freqs.getSymbolLimit()
+        require(numSym >= 1) { "At least 1 symbol needed" }
+
+        frequencies = IntArray(numSym)
+        total = 0
+        for (i in frequencies.indices) {
+            val x = freqs.get(i)
+            require(x >= 0) { "Negative frequency" }
+            frequencies[i] = x
+            total += x
+        }
+    }
+
+
+    /*---- Methods ----*/
+    /**
+     * Returns the number of symbols in this frequency table, which is at least 1.
+     * @return the number of symbols in this frequency table
+     */
+    override fun getSymbolLimit(): Int {
+        return frequencies.size
+    }
+
+
+    /**
+     * Returns the frequency of the specified symbol. The returned value is at least 0.
+     * @param symbol the symbol to query
+     * @return the frequency of the specified symbol
+     * @throws IllegalArgumentException if `symbol` &lt; 0 or `symbol`  `getSymbolLimit()`
+     */
+    override fun get(symbol: Int): Int {
+        checkSymbol(symbol)
+        return frequencies[symbol]
+    }
+
+
+    /**
+     * Sets the frequency of the specified symbol to the specified value. The frequency value
+     * must be at least 0. If an exception is thrown, then the state is left unchanged.
+     * @param symbol the symbol to set
+     * @param freq the frequency value to set
+     * @throws IllegalArgumentException if `symbol` &lt; 0 or `symbol`  `getSymbolLimit()`
+     * @throws ArithmeticException if this set request would cause the total to exceed `Integer.MAX_VALUE`
+     */
+    override fun set(symbol: Int, freq: Int) {
+        checkSymbol(symbol)
+        require(freq >= 0) { "Negative frequency" }
+
+        val temp = total - frequencies[symbol]
+        check(temp >= 0)
+        //if (temp < 0) throw java.lang.AssertionError()
+        total = java.lang.Math.addExact(temp, freq)
+        frequencies[symbol] = freq
+        cumulative = intArrayOf()
+    }
+
+
+    /**
+     * Increments the frequency of the specified symbol.
+     * @param symbol the symbol whose frequency to increment
+     * @throws IllegalArgumentException if `symbol` &lt; 0 or `symbol`  `getSymbolLimit()`
+     */
+    override fun increment(symbol: Int) {
+        checkSymbol(symbol)
+        check(frequencies[symbol] != Int.Companion.MAX_VALUE) { "Arithmetic overflow" }
+        // if (frequencies[symbol] == Int.Companion.MAX_VALUE) throw java.lang.ArithmeticException("Arithmetic overflow")
+        total = java.lang.Math.addExact(total, 1)
+        frequencies[symbol]++
+        cumulative = intArrayOf()
+    }
+
+
+    /**
+     * Returns the total of all symbol frequencies. The returned value is at
+     * least 0 and is always equal to `getHigh(getSymbolLimit() - 1)`.
+     * @return the total of all symbol frequencies
+     */
+    override fun getTotal(): Int {
+        return total
+    }
+
+
+    /**
+     * Returns the sum of the frequencies of all the symbols strictly
+     * below the specified symbol value. The returned value is at least 0.
+     * @param symbol the symbol to query
+     * @return the sum of the frequencies of all the symbols below `symbol`
+     * @throws IllegalArgumentException if `symbol` &lt; 0 or `symbol`  `getSymbolLimit()`
+     */
+    override fun getLow(symbol: Int): Int {
+        checkSymbol(symbol)
+        if (cumulative == null) initCumulative()
+        return cumulative!![symbol]
+    }
+
+
+    /**
+     * Returns the sum of the frequencies of the specified symbol
+     * and all the symbols below. The returned value is at least 0.
+     * @param symbol the symbol to query
+     * @return the sum of the frequencies of `symbol` and all symbols below
+     * @throws IllegalArgumentException if `symbol` &lt; 0 or `symbol`  `getSymbolLimit()`
+     */
+    override fun getHigh(symbol: Int): Int {
+        checkSymbol(symbol)
+        if (cumulative.isEmpty()) initCumulative()
+        return cumulative[symbol + 1]
+    }
+
+
+    // Recomputes the array of cumulative symbol frequencies.
+    private fun initCumulative() {
+        cumulative = IntArray(frequencies.size + 1)
+        var sum = 0
+        for (i in frequencies.indices) {
+            // This arithmetic should not throw an exception, because invariants are being maintained
+            // elsewhere in the data structure. This implementation is just a defensive measure.
+            sum = java.lang.Math.addExact(frequencies[i], sum)
+            cumulative[i + 1] = sum
+        }
+        check(!(sum != total))
+        //if (sum != total) throw java.lang.AssertionError()
+    }
+
+
+    // Returns silently if 0 <= symbol < frequencies.length, otherwise throws an exception.
+    private fun checkSymbol(symbol: Int) {
+        require(0 <= symbol && symbol < frequencies.size) { "Symbol out of range" }
+    }
+
+
+    /**
+     * Returns a string representation of this frequency table,
+     * useful for debugging only, and the format is subject to change.
+     * @return a string representation of this frequency table
+     */
+    override fun toString(): String {
+        val sb: StringBuilder = StringBuilder()
+        //for (i in frequencies.indices) sb.append(String.format("%d\t%d%n", i, frequencies[i]))
+        return sb.toString()
+    }
+}
