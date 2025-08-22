74bc69fc3c7d000387f8868056e8c8274336f70b
Kristoffer Paulsson <kristoffer.paulsson@talenten.se>
2025-08-22 22:33:39 +0200

    Restructure

--- library/src/commonMain/kotlin/org/example/arithmetic/BitOutputBuffer.kt
+++ library/src/commonMain/kotlin/org/example/arithmetic/BitOutputBuffer.kt
@@ -1,24 +1,28 @@
 package org.example.arithmetic
 
-public class BitOutputBuffer {
-    private val bytes = mutableListOf<Byte>()
+public class BitOutputBuffer(size: Int) {
+    private val bytes = ByteArray(size)
+    private var byteIndex = 0
     private var currentByte = 0
     private var numBitsFilled = 0
 
     public fun writeBit(bit: Int) {
-        currentByte = currentByte shl 1 or bit
+        if (byteIndex >= bytes.size) return // Ignore if buffer is full
+        currentByte = currentByte shl 1 or (bit and 1)
         numBitsFilled++
         if (numBitsFilled == 8) {
-            bytes.add(currentByte.toByte())
+            bytes[byteIndex++] = currentByte.toByte()
             numBitsFilled = 0
             currentByte = 0
         }
     }
 
     public fun toByteArray(): ByteArray {
-        if (numBitsFilled > 0) {
-            bytes.add((currentByte shl (8 - numBitsFilled)).toByte())
+        var length = byteIndex
+        if (numBitsFilled > 0 && byteIndex < bytes.size) {
+            bytes[byteIndex++] = (currentByte shl (8 - numBitsFilled)).toByte()
+            length = byteIndex
         }
-        return bytes.toByteArray()
+        return bytes.copyOf(length)
     }
 }
