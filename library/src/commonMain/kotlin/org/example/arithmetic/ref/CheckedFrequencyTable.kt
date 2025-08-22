ae337ce920fcaf40b6091e7cdbca8c469cc18f6a
Kristoffer Paulsson <kristoffer.paulsson@talenten.se>
2025-08-22 23:11:28 +0200

    Added frequency table

---
+++ library/src/commonMain/kotlin/org/example/arithmetic/ref/CheckedFrequencyTable.kt
@@ -0,0 +1,90 @@
+package org.example.arithmetic.ref
+
+
+/**
+ * A wrapper that checks the preconditions (arguments) and postconditions (return value)
+ * of all the frequency table methods. Useful for finding faults in a frequency table
+ * implementation. However, arithmetic overflow conditions are not checked.
+ */
+public class CheckedFrequencyTable(freq: FrequencyTable) : FrequencyTable {
+    /*---- Fields ----*/ // The underlying frequency table that holds the data (not null).
+    private val freqTable: FrequencyTable
+
+
+    /*---- Constructor ----*/
+    init {
+        freqTable = java.util.Objects.requireNonNull<FrequencyTable?>(freq)
+    }
+
+
+    /*---- Methods ----*/
+    override fun getSymbolLimit(): Int {
+        val result = freqTable.getSymbolLimit()
+        if (result <= 0) throw java.lang.AssertionError("Non-positive symbol limit")
+        return result
+    }
+
+
+    override fun get(symbol: Int): Int {
+        val result = freqTable.get(symbol)
+        if (!isSymbolInRange(symbol)) throw java.lang.AssertionError("IllegalArgumentException expected")
+        if (result < 0) throw java.lang.AssertionError("Negative symbol frequency")
+        return result
+    }
+
+
+    override fun getTotal(): Int {
+        val result = freqTable.getTotal()
+        if (result < 0) throw java.lang.AssertionError("Negative total frequency")
+        return result
+    }
+
+
+    override fun getLow(symbol: Int): Int {
+        if (isSymbolInRange(symbol)) {
+            val low = freqTable.getLow(symbol)
+            val high = freqTable.getHigh(symbol)
+            if (!(0 <= low && low <= high && high <= freqTable.getTotal())) throw java.lang.AssertionError("Symbol low cumulative frequency out of range")
+            return low
+        } else {
+            freqTable.getLow(symbol)
+            error("IllegalArgumentException expected")
+            //throw java.lang.AssertionError()
+        }
+    }
+
+
+    override fun getHigh(symbol: Int): Int {
+        if (isSymbolInRange(symbol)) {
+            val low = freqTable.getLow(symbol)
+            val high = freqTable.getHigh(symbol)
+            if (!(0 <= low && low <= high && high <= freqTable.getTotal())) throw java.lang.AssertionError("Symbol high cumulative frequency out of range")
+            return high
+        } else {
+            freqTable.getHigh(symbol)
+            throw java.lang.AssertionError("IllegalArgumentException expected")
+        }
+    }
+
+
+    override fun toString(): String {
+        return "CheckedFrequencyTable (" + freqTable.toString() + ")"
+    }
+
+
+    override fun set(symbol: Int, freq: Int) {
+        freqTable.set(symbol, freq)
+        if (!isSymbolInRange(symbol) || freq < 0) throw java.lang.AssertionError("IllegalArgumentException expected")
+    }
+
+
+    override fun increment(symbol: Int) {
+        freqTable.increment(symbol)
+        if (!isSymbolInRange(symbol)) throw java.lang.AssertionError("IllegalArgumentException expected")
+    }
+
+
+    private fun isSymbolInRange(symbol: Int): Boolean {
+        return 0 <= symbol && symbol < getSymbolLimit()
+    }
+}
