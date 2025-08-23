b17b8cc157955530dac8622b02103c2915fff9de
Kristoffer Paulsson <kristoffer.paulsson@talenten.se>
2025-08-23 14:49:53 +0200

    Small adaptions

--- library/src/jvmMain/kotlin/org/example/arithmetic/BitInputStream.kt
+++ library/src/jvmMain/kotlin/org/example/arithmetic/BitInputStream.kt
@@ -21,9 +21,7 @@
 package org.example.arithmetic
 
 import java.io.EOFException
-import java.io.IOException
 import java.io.InputStream
-import java.util.Objects
 
 /**
  * A stream of bits that can be read. Because they come from an underlying byte stream,
@@ -31,8 +29,7 @@ import java.util.Objects
  * Mutable and not thread-safe.
  * @see BitOutputStream
  */
-public class BitInputStream public constructor(`in`: InputStream) : AutoCloseable {
-    /*---- Fields ----*/ // The underlying byte stream to read from (not null).
+public class BitInputStream public constructor(inp: InputStream) : AutoCloseable {
     private val input: InputStream
 
     // Either in the range [0x00, 0xFF] if bits are available, or -1 if end of stream is reached.
@@ -41,25 +38,20 @@ public class BitInputStream public constructor(`in`: InputStream) : AutoCloseabl
     // Number of remaining bits in the current byte, always between 0 and 7 (inclusive).
     private var numBitsRemaining = 0
 
-
-    /*---- Constructor ----*/ /**
+    /**
      * Constructs a bit input stream based on the specified byte input stream.
-     * @param in the byte input stream
+     * @param `in` the byte input stream
      * @throws NullPointerException if the input stream is `null`
      */
     init {
-        input = Objects.requireNonNull(`in`)!!
+        input = inp
     }
 
-
-    /*---- Methods ----*/
     /**
      * Reads a bit from this stream. Returns 0 or 1 if a bit is available, or -1 if
      * the end of stream is reached. The end of stream always occurs on a byte boundary.
      * @return the next bit of 0 or 1, or -1 for the end of stream
-     * @throws IOException if an I/O exception occurred
      */
-    @Throws(IOException::class)
     public fun read(): Int {
         if (currentByte == -1) return -1
         if (numBitsRemaining == 0) {
@@ -72,27 +64,21 @@ public class BitInputStream public constructor(`in`: InputStream) : AutoCloseabl
         return (currentByte ushr numBitsRemaining) and 1
     }
 
-
     /**
      * Reads a bit from this stream. Returns 0 or 1 if a bit is available, or throws an `EOFException`
      * if the end of stream is reached. The end of stream always occurs on a byte boundary.
      * @return the next bit of 0 or 1
-     * @throws IOException if an I/O exception occurred
      * @throws EOFException if the end of stream is reached
      */
-    @Throws(IOException::class)
     public fun readNoEof(): Int {
         val result = read()
         if (result != -1) return result
         else throw EOFException()
     }
 
-
     /**
      * Closes this stream and the underlying input stream.
-     * @throws IOException if an I/O exception occurred
      */
-    @Throws(IOException::class)
     public override fun close() {
         input.close()
         currentByte = -1
