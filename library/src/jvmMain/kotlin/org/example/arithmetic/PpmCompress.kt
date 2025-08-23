1ed7e962668176f24858cc50d83a04721b1d5689
Kristoffer Paulsson <kristoffer.paulsson@talenten.se>
2025-08-23 14:55:30 +0200

    Small adaptions

--- library/src/jvmMain/kotlin/org/example/arithmetic/PpmCompress.kt
+++ library/src/jvmMain/kotlin/org/example/arithmetic/PpmCompress.kt
@@ -25,7 +25,6 @@ import java.io.BufferedOutputStream
 import java.io.File
 import java.io.FileInputStream
 import java.io.FileOutputStream
-import java.io.IOException
 import java.io.InputStream
 import java.util.Arrays
 
@@ -43,8 +42,6 @@ public object PpmCompress {
     // Must be at least -1 and match PpmDecompress. Warning: Exponential memory usage at O(257^n).
     private const val MODEL_ORDER = 3
 
-
-    @Throws(IOException::class)
     public fun main(args: Array<String>) {
         // Handle command line arguments
         if (args.size != 2) {
@@ -55,17 +52,16 @@ public object PpmCompress {
         val inputFile: File = File(args[0])
         val outputFile: File = File(args[1])
 
-        BufferedInputStream(FileInputStream(inputFile)).use { `in` ->
+        BufferedInputStream(FileInputStream(inputFile)).use { inp ->
             BitOutputStream(BufferedOutputStream(FileOutputStream(outputFile))).use { out ->
-                compress(`in`, out)
+                compress(inp, out)
             }
         }
     }
 
 
     // To allow unit testing, this method is package-private instead of private.
-    @Throws(IOException::class)
-    public fun compress(`in`: InputStream, out: BitOutputStream) {
+    public fun compress(inp: InputStream, out: BitOutputStream) {
         // Set up encoder and model. In this PPM model, symbol 256 represents EOF;
         // its frequency is 1 in the order -1 context but its frequency
         // is 0 in all other contexts (which have non-negative order).
@@ -75,7 +71,7 @@ public object PpmCompress {
 
         while (true) {
             // Read and encode one byte
-            val symbol: Int = `in`.read()
+            val symbol: Int = inp.read()
             if (symbol == -1) break
             encodeSymbol(model, history, symbol, enc)
             model.incrementContexts(history, symbol)
@@ -92,8 +88,6 @@ public object PpmCompress {
         enc.finish() // Flush remaining code bits
     }
 
-
-    @Throws(IOException::class)
     private fun encodeSymbol(model: PpmModel, history: IntArray, symbol: Int, enc: ArithmeticEncoder) {
         // Try to use highest order context that exists based on the history suffix, such
         // that the next symbol has non-zero frequency. When symbol 256 is produced at a context
