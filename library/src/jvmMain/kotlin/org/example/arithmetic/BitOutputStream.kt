599ca9cc3958be412752ecb00000819d9d3ee76e
Kristoffer Paulsson <kristoffer.paulsson@talenten.se>
2025-08-23 14:50:51 +0200

    Small adaptions

--- library/src/jvmMain/kotlin/org/example/arithmetic/BitOutputStream.kt
+++ library/src/jvmMain/kotlin/org/example/arithmetic/BitOutputStream.kt
@@ -31,7 +31,6 @@ import java.util.Objects
  * @see BitInputStream
  */
 public class BitOutputStream public constructor(out: OutputStream) : AutoCloseable {
-    /*---- Fields ----*/ // The underlying byte stream to write to (not null).
     private val output: OutputStream
 
     // The accumulated bits for the current byte, always in the range [0x00, 0xFF].
@@ -40,24 +39,18 @@ public class BitOutputStream public constructor(out: OutputStream) : AutoCloseab
     // Number of accumulated bits in the current byte, always between 0 and 7 (inclusive).
     private var numBitsFilled = 0
 
-
-    /*---- Constructor ----*/ /**
+    /**
      * Constructs a bit output stream based on the specified byte output stream.
      * @param out the byte output stream
-     * @throws NullPointerException if the output stream is `null`
      */
     init {
-        output = Objects.requireNonNull(out)!!
+        output = out
     }
 
-
-    /*---- Methods ----*/
     /**
      * Writes a bit to the stream. The specified bit must be 0 or 1.
      * @param b the bit to write, which must be 0 or 1
-     * @throws IOException if an I/O exception occurred
      */
-    @Throws(IOException::class)
     public fun write(b: Int) {
         require(!(b != 0 && b != 1)) { "Argument must be 0 or 1" }
         currentByte = (currentByte shl 1) or b
@@ -74,9 +67,7 @@ public class BitOutputStream public constructor(out: OutputStream) : AutoCloseab
      * Closes this stream and the underlying output stream. If called when this
      * bit stream is not at a byte boundary, then the minimum number of "0" bits
      * (between 0 and 7 of them) are written as padding to reach the next byte boundary.
-     * @throws IOException if an I/O exception occurred
      */
-    @Throws(IOException::class)
     public override fun close() {
         while (numBitsFilled != 0) write(0)
         output.close()
