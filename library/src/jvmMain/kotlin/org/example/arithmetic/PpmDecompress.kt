e291d8dcd8a9a9f4df43bdc1903f04541a8e46ee
Kristoffer Paulsson <kristoffer.paulsson@talenten.se>
2025-08-23 14:56:40 +0200

    Small adaptions

--- library/src/jvmMain/kotlin/org/example/arithmetic/PpmDecompress.kt
+++ library/src/jvmMain/kotlin/org/example/arithmetic/PpmDecompress.kt
@@ -25,7 +25,6 @@ import java.io.BufferedOutputStream
 import java.io.File
 import java.io.FileInputStream
 import java.io.FileOutputStream
-import java.io.IOException
 import java.io.OutputStream
 import java.util.Arrays
 
@@ -40,8 +39,6 @@ public object PpmDecompress {
     // Must be at least -1 and match PpmCompress. Warning: Exponential memory usage at O(257^n).
     private const val MODEL_ORDER = 3
 
-
-    @Throws(IOException::class)
     public fun main(args: Array<String>) {
         // Handle command line arguments
         if (args.size != 2) {
@@ -52,21 +49,19 @@ public object PpmDecompress {
         val inputFile: File = File(args[0])
         val outputFile: File = File(args[1])
 
-        BitInputStream(BufferedInputStream(FileInputStream(inputFile))).use { `in` ->
+        BitInputStream(BufferedInputStream(FileInputStream(inputFile))).use { inp ->
             BufferedOutputStream(FileOutputStream(outputFile)).use { out ->
-                decompress(`in`, out)
+                decompress(inp, out)
             }
         }
     }
 
-
     // To allow unit testing, this method is package-private instead of private.
-    @Throws(IOException::class)
-    public fun decompress(`in`: BitInputStream, out: OutputStream) {
+    public fun decompress(inp: BitInputStream, out: OutputStream) {
         // Set up decoder and model. In this PPM model, symbol 256 represents EOF;
         // its frequency is 1 in the order -1 context but its frequency
         // is 0 in all other contexts (which have non-negative order).
-        val dec: ArithmeticDecoder = ArithmeticDecoder(32, `in`)
+        val dec: ArithmeticDecoder = ArithmeticDecoder(32, inp)
         val model: PpmModel = PpmModel(MODEL_ORDER, 257, 256)
         var history = IntArray(0)
 
@@ -87,8 +82,6 @@ public object PpmDecompress {
         }
     }
 
-
-    @Throws(IOException::class)
     private fun decodeSymbol(dec: ArithmeticDecoder, model: PpmModel, history: IntArray): Int {
         // Try to use highest order context that exists based on the history suffix. When symbol 256
         // is consumed at a context at any non-negative order, it means "escape to the next lower order
