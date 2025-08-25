76cfef36aaada6485af2e3b858b90f7250efc00b
Kristoffer Paulsson <kristoffer.paulsson@talenten.se>
2025-08-25 12:59:50 +0200

    Small adaptions

---
+++ library/src/commonMain/kotlin/org/example/arithmetic/io/ByteOutput.kt
@@ -0,0 +1,20 @@
+/**
+ * Additional IO interfaces for bytewise operations.
+ *
+ * Copyright (c) 2025 by Kristoffer Paulsson <kristoffer.paulsson@talenten.se>.
+ *
+ * This software is available under the terms of the MIT license.
+ * The legal terms are attached to the LICENSE file and are made
+ * available on:
+ *
+ *      https://opensource.org/licenses/MIT
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ * Contributors:
+ *      Kristoffer Paulsson - initial implementation
+ */
+package org.example.arithmetic.io
+
+public interface ByteOutput {
+}
